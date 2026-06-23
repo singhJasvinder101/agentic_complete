7cd198375abab307766dc3c9be95ac4c018bdfc2
Jasvinder Singh <jasvinder.singh@omniful.ai>
2026-06-23 17:50:45 +0530

    added rag

---
+++ rag/embedding.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
+
+script_dir = Path(__file__).parent
+pdf_path = script_dir / "sample.pdf"
+
+loader = PyPDFLoader(str(pdf_path))
+
+# gives every single page
+docs = loader.load()
+
+#print(docs[0])
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = text_splitter.split_documents(documents=docs)
+
+#print(chunks[0])
+#print(chunks[1])
+
+embedding_model = OpenAIEmbeddings(
+    model = "text-embedding-3-large",
+)
+
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="rag-collection"
+)
+
+print("Indexing done")
---
+++ rag/query.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
+from langchain_openai import OpenAIEmbeddings
+from openai import OpenAI
+
+load_dotenv()
+
+
+embedding_model = OpenAIEmbeddings(
+    model = "text-embedding-3-large",
+)
+
+vector_store = QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="rag-collection",
+    embedding=embedding_model,
+)
+
+client = OpenAI()
+
+
+while True:
+    query = input("Enter your question: ")
+    if query.lower() == "exit":
+        break
+
+    
+    # relevant chunks from db
+    #docs = vector_store.similarity_search("What is the main topic of the document?", k=3)
+    docs = vector_store.similarity_search(query)
+
+    #for doc in docs:
+        #print(doc.page_content)
+        #print(doc.metadata)
+        #print("Page Number:", doc.metadata["page"])
+
+    context = "\n\n\n".join([f"Page Number: {doc.metadata['page']}\nContent: {doc.page_content}" for doc in docs])
+
+    SYSTEM_PROMPT = """
+    You are a helpful assistant that answers questions based on the context provided from pdf file. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    provide the page_content and page_number of the relevant chunks from the pdf file in your answer.
+
+    Navigate the user to page number for more information.
+    Context: {context}
+
+    """
+
+    prompt = SYSTEM_PROMPT.format(context=context)
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": query}
+        ]
+    )
+
+    print("Answer:", response.choices[0].message.content)
